be575d6b0658964d2228294a1629a9aabb41ab3c
Instructor <vokunev@amazon.com>
2020-07-09 16:06:40 -0700

    Dropped the query option

--- translate/translate-text.sh
+++ translate/translate-text.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws translate translate-text --text "Hello and welcome to Amazon Cloud" --query TranslatedText --output json --source-language-code en --target-language-code es 
+aws translate translate-text --text "Hello and welcome to Amazon Cloud" --output json --source-language-code en --target-language-code es 
